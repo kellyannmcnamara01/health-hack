fce4785d87f963051e01e518a05e3210bd1f556f
Josh McCormick <jmccormick1024@gmail.com>
2017-03-10 14:00:54 -0500

    Change routing for index file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'Header.php';
-require_once 'sidebar.php';
+require_once 'Common Views/Header.php';
+require_once 'Common Views/sidebar.php';
 ?>
 
 
@@ -94,5 +94,5 @@ require_once 'sidebar.php';
     </main>
     <!-- 02-2-3. Footer -->
    <?php
-    require_once 'Footer.php';
+    require_once 'Common Views/Footer.php';
     ?>
