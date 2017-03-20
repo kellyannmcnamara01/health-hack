8bb4fdcf56500cebba74968c37b22250dd515bfd
kellyannmcnamara01 <kellyannmcnamara01@gmail.com>
2017-03-20 09:07:25 -0400

    fixed my bug

--- Models/GroceryListDAO.php
+++ Models/GroceryListDAO.php
@@ -6,14 +6,17 @@
  * Date: 2017-03-12
  * Time: 12:32 PM
  */
+
+
+
+
 class GroceryListDAO
 {
     public function populateGroceryLists($db) {
         $query_groceryLists = "SELECT * FROM GROCERY_LISTS";
         $pdo_statement = $db->prepare($query_groceryLists);
-        $pdo_statement = execute();
+        $pdo_statement->execute();
         $groceryLists = $pdo_statement->fetchAll(PDO::FETCH_OBJ);
         return $groceryLists;
     }
 }
-
