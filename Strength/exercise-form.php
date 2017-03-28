5c26eb4947fdb8163a6f384a907606b314d78bb3
Josh McCormick <jmccormick1024@gmail.com>
2017-03-28 10:47:01 -0400

    logic for inserting completed workouts

---
+++ Strength/exercise-form.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JoshMcCormick
+ * Date: 2017-03-28
+ * Time: 9:41 AM
+ */
+
+?>
+<?php  foreach ($strength_Workout as $key) :?>
+    <tr><input type="hidden" name="exercise_id[]" value="<?php echo $key['exercise_id'];?>"/>
+        <input type="hidden" name="strength_id[]" value="<?php echo $key['strength_id'];?>"/>
+        <input type="hidden" name="exercise_name[]" value="<?php echo $key['exercise_name']?>"/>
+        <td><?php echo $key['exercise_name'];?></td>
+        <td><select name="weight[]">
+                <?php
+                foreach (range(0, 200, 5) as $i):
+                    ?>
+                    <option
+                        value="<?php
+                        echo $i;
+                        ?>"><?php
+                        echo $i;
+                        ?></option>
+                <?php endforeach;
+                ?>
+            </select>
+        </td>
+        <td><select name="set_1[]">
+                <?php foreach (range(0, 15, 1) as $i):?>
+                    <option value="<?php echo $i;?>"><?php echo $i;?></option><?php endforeach;?>
+            </select>
+        </td>
+        <td><select name="set_2[]">
+                <?php foreach (range(0, 15, 1) as $i):?>
+                    <option value="<?php echo $i;?>"><?php echo $i;?></option><?php endforeach;?>
+            </select>
+        </td>
+        <td><select name="set_3[]">
+                <?php foreach (range(0, 15, 1) as $i):?>
+                    <option value="<?php echo $i;?>"><?php echo $i;?></option><?php endforeach;?>
+            </select>
+
+        </td>
+    </tr>
+
+
+<?php  endforeach;?>
--- Strength/log-strength.php
+++ Strength/log-strength.php
@@ -50,29 +50,79 @@ if (isset($_POST['load_strength'])){
 
         //now let's get all the exercises associated with that strength workout.
 
-        $get_1_Strength = new StrengthWorkoutDAO();
+        $get_Strength = new StrengthWorkoutDAO();
         $strength_Workout = $get_strength->get1StrengthWorkout($conn, $selected_Strength_Workout);
-       /* echo "<li>" .  $strength_Workout[0]['strength_id'] .  "</li>" ;
-        echo  "<li>" . $strength_Workout[0]['user_id'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[0]['strength_workout_name'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[0]['exercise_id'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[0]['exercise_name'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[0]['strength_workout_id'].  "</li>" ;
-        echo "<li>" .  $strength_Workout[1]['strength_id'] .  "</li>" ;
-        echo  "<li>" . $strength_Workout[1]['user_id'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[1]['strength_workout_name'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[1]['exercise_id'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[1]['exercise_name'].  "</li>" ;
-        echo  "<li>" . $strength_Workout[1]['strength_workout_id'].  "</li>" ;*/
-      //  print_r($strength_Workout);
-        /*for ($i = 0; $i < count($strength_Workout); $i++){
-            echo $strength_Workout[$i]['exercise_name'];
-        } */
-
 
 
 
     }
+}
+if (isset($_POST['save_strength'])){
+    //re-capture the form so it still displays if the user presses this button
+
+    //grab all the strength workouts associated with the id, and store it for access in a drop down list.
+//create a new strength workout and set it's id equal to that of the user signed in.
+
+    require_once '../Models/StrengthWorkout.php';
+    $sw = new StrengthWorkout();
+    $sw->setUserId($user_id);
+
+//create the db connection
+    require_once '../Models/Database.php';
+    $db = new Database();
+    $conn = $db->getDb();
+$strength_id = filter_input(INPUT_POST,'strength_workout');
+//validate it to make sure it's not zero.
+
+require_once '../Models/Validation.php';
+$v = new Validation();
+if ($v->testZero($strength_id) == false){
+    $strength_workout_error = "You must select a strength workout!";
+}
+else {
+    //now create the strength workout object
+
+    $selected_Strength_Workout = new StrengthWorkout();
+    $selected_Strength_Workout->setUserId($user_id);
+    $selected_Strength_Workout->setId($strength_id);
+
+    //now let's get all the exercises associated with that strength workout.
+
+    $get_Strength = new StrengthWorkoutDAO();
+    $strength_Workout = $get_strength->get1StrengthWorkout($conn, $selected_Strength_Workout);
+}
+
+//grab all of our strength workouts passing in the connection and strength workout object
+    require_once '../Models/StrengthWorkoutDAO.php';
+    $get_strength = new StrengthWorkoutDAO();
+    $strength_workouts = $get_strength->getStrengthWorkouts($conn, $sw);
+
+    $completed_exercise_id = array($_POST['exercise_id'])[0];
+    $completed_strength_id = array($_POST['strength_id'])[0];
+    $exercise_name = array($_POST['exercise_name'])[0];
+    $weight = array($_POST['weight'])[0];
+    $set_1 = array($_POST['set_1'])[0];
+    $set_2 = array ($_POST['set_2'])[0];
+    $set_3 = array($_POST['set_3'])[0];
+
+    for ($i = 0; $i < count($completed_exercise_id); $i++){
+        $query = "INSERT INTO COMPLETED_STRENGTH_EXERCISES 
+        (exercise_id, strength_id, exercise_name, weight, set_1, set_2, set_3)
+        VALUES (:completed_exercise_id, :completed_strength_id, :exercise_name, :weight,:set_1, :set_2, :set_3 )";
+        $statement= $conn->prepare($query);
+        $statement->bindValue(':completed_exercise_id', $completed_exercise_id[$i]);
+        $statement->bindValue(':completed_strength_id', $completed_strength_id[$i]);
+        $statement->bindValue('exercise_name', $exercise_name[$i]);
+        $statement->bindValue(':weight', $weight[$i]);
+        $statement->bindValue(':set_1', $set_1[$i]);
+        $statement->bindValue(':set_2', $set_2[$i]);
+        $statement->bindValue(':set_3', $set_3[$i]);
+        $statement->execute();
+        $statement->closeCursor();
+    }
+
+    //now that we've grabbed all the elements we need, we must insert them into the completed strength workouts table in a foreach loop
+
 }
 ?>
 <div id="main-content" class="col-md-9 col-sm-12 col-12 row">
@@ -93,10 +143,15 @@ if (isset($_POST['load_strength'])){
                 foreach ($strength_workouts as $strength):
                     ?>
                     <option <?php
-                    if (isset($strength_workout) && $strength_workout['strength_workout_name'] == $strength['strength_workout_name']) {
-                        echo 'selected';
+                    if (isset($strength_Workout)) {
+                        foreach ($strength_Workout as $key){
+                            if ($key['strength_id'] == $strength['strength_id']){
+                                echo 'selected ';
+                            }
+
+                        }
                     }
-                    ?> ) value="<?php
+                    ?>  value="<?php
                     echo $strength['strength_id'];
                     ?>"><?php
                         echo $strength['strength_workout_name'];
@@ -111,7 +166,7 @@ if (isset($_POST['load_strength'])){
             <input  type="submit" name="load_strength" value="Load Details" class="formSubmit col-md-3 col-sm-6 col-xs-1 text-center offset-md-5 offset-sm-3 "/>
         </div>
         <h3 class="offset-md-1 spacing">Log your workout here </h3>
-            <div class="table-responsive">
+            <div class="table-responsive big-spacing">
                 <table class="table">
                     <thead>
                     <tr>
@@ -123,48 +178,16 @@ if (isset($_POST['load_strength'])){
                     </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($strength_Workout as $key):?>
-                    <tr><input type="hidden" name="exercise_id[]" value="<?php echo $key['exercise_id'];?>"/>
-                        <input type="hidden" name="strength_id[]" value="<?php echo $key['strength_id'];?>"/>
-                        <td><?php echo $key['exercise_name'];?></td>
-                        <td><select name="weight[]">
-                                <?php
-                                foreach (range(0, 200, 5) as $i):
-                                    ?>
-                                    <option
-                                value="<?php
-                                    echo $i;
-                                    ?>"><?php
-                                        echo $i;
-                                        ?></option>
-                                    <?php
-                                endforeach;
-                                ?>
-                            </select>
-                        </td>
-                        <td><select name="set_1[]">
-                                <?php foreach (range(0, 15, 1) as $i):?>
-                                <option value="<?php echo $i;?>"><?php echo $i;?></option><?php endforeach;?>
-                            </select>
-                        </td>
-                        <td><select name="set_2[]">
-                                <?php foreach (range(0, 15, 1) as $i):?>
-                                <option value="<?php echo $i;?>"><?php echo $i;?></option><?php endforeach;?>
-                            </select>
-                        </td>
-                        <td><select name="set_3[]">
-                                <?php foreach (range(0, 15, 1) as $i):?>
-                                <option value="<?php echo $i;?>"><?php echo $i;?></option><?php endforeach;?>
-                            </select>
-
-                        </td>
-                    </tr>
-
-                    <?php endforeach;?>
+                    <?php if (isset($strength_Workout)){
+                        require_once 'exercise-form.php';
+                    }?>
 
 
                     </tbody>
                 </table>
+            </div>
+            <div class="form-field big-spacing">
+              <input  type="submit" name="save_strength" value="Save Workout" class="formSubmit col-md-3 col-sm-6 col-xs-1 text-center offset-md-5 offset-sm-3    "/>
             </div>
         </form>
     </div>
@@ -172,5 +195,5 @@ if (isset($_POST['load_strength'])){
 </main>
 <?php
 require_once '../Common Views/Footer.php';
-
 ?>
+
