ef75ba533b0f144ebc2f78a5d5145942cf453cb5
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2017-04-05 10:50:47 -0400

    added Dashboard/index.php

--- Common Views/sidebar.php
+++ Common Views/sidebar.php
@@ -19,12 +19,12 @@
         <!-- 02-2-1-3. Links -->
         <div class="nav-links">
             <ul>
-                <li class="link-hover"><a href="#" title="Dashboard Link">Dashboard</a></li>
+                <li class="link-hover"><a href="Dashboard/index.php" title="Dashboard Link">Dashboard</a></li>
                 <li class="link-hover"><a href="/health-hack/Calendar" title="Calendar Link">Calendar</a></li>
                 <li class="link-hover"><a href="/health-hack/FitnessProgress" title="Fitness Link">Fitness</a></li>
                 <li class="link-hover"><a href="/health-hack/GymLocator" title="Gym Locator Link">Gym Locator</a></li>
                 <li class="link-hover"><a href="/health-hack/GroceryList" title="Nutrition Link">Nutrition</a></li>
-                <li class="link-hover"><a href="logout.php" title="Log Out Link" >Log Out</a></li>
+                <li class="link-hover"><a href="../logout.php" title="Log Out Link" >Log Out</a></li>
             </ul>
         </div>
     </nav>
---
+++ Dashboard/index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryanstephens
+ * Date: 2017-04-05
+ * Time: 9:41 AM
+ * Dashboard/index.php ==> allows users to change their profile information used for Health-hack (weight, profile image); also allows user to input their age
+ */
+//start session
+session_start();
+
+// include project files (check if files are already included, if they are, won't include require them again)
+require_once '../Models/Signup.php';
+require_once '../Common Views/Header.php';
+//require_once '../Common Views/sidebar.php';
+
+// est. variable that contains session variable for email
+$user = $_SESSION['user'];
+
+//new instance of Signup()
+$db = new Signup();
+// call userInfo() method
+$userId = $db->userInfo($user);
+//grab  user id, username
+$id = $userId->user_id;
+$userName = $userId->first_name;
+
+?>
+<div class="container">
+    <div id="main-content" class="col-12">
+        <h1 class="h1 col col-md-8"><?php echo $userName;?>'s Dashboard</h1>
+        <p class="text-primary col col-md-8">Update your age, weight, and profile image below</p>
+    <!-- form to update user profile information -->
+    <!-- since user will upload an image, set enctype on form -->
+        <form action="index.php" method="post" enctype="multipart/form-data" id="updateProfileInformation">
+            <div class="form-field col col-md-8">
+                <label class="formLabel">Age</label>
+                <input class="textInput" placeholder="Age" name="ProfileAge">
+            </div>
+            <div class="form-field col col-md-8">
+                <label class="formLabel">Weight</label>
+                <input class="textInput" placeholder="Weight" name="ProfileWeight">
+            </div>
+            <div class="form-field col col-md-8">
+                <label class="formLabel">Upload Profile Image</label>
+                <input class="textInput" type="file">
+            </div>
+            <div class="form-field col col-md-8">
+                <input type="submit" class="formSubmit" name="Submit" value="Submit">
+            </div>
+        </form>
+    </div> <!-- end of main-content div-->
+</div> <!-- end of container div-->
+
+<?php
+require_once '../Common Views/Footer.php';
+?>
