bda5ab9d0d893268a97c2b111d55b935f789d0fb
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2017-04-17 10:14:51 -0400

    added session storage for redirect.php

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -16,26 +16,7 @@ require_once 'processImg.php';
 
 require_once '../redirect.php';
 
-$user = $_SESSION['user'];
 
-// call userInfo() method using user_id from $_SESSION
-$db = new Signup();
-
-$userId = $db->userInfo($user);
-
-//grab  user id, username
-$id = $userId->user_id;
-$userFirst = $userId->first_name;
-$userName = $userId->first_name . ' ' . $userId->last_name;
-$userEmail = $userId->email;
-
-$profile = new Profile();
-
-$results = $profile->getUserProfileIinfo($id);
-$age = $results->age;
-$weight = $results->weight;
-
-$img = $profile->getUserProfileImg($id)->profile_image;
 
 //print_r($img);
 if (isset($_POST['profileSubmit']))
--- redirect.php
+++ redirect.php
@@ -10,3 +10,23 @@ if(!isset($_SESSION['user'])){
     header("Location:/health-hack/landing.php");
 }
 
+$user = $_SESSION['user'];
+
+// call userInfo() method using user_id from $_SESSION
+$db = new Signup();
+
+$userId = $db->userInfo($user);
+
+//grab  user id, username
+$id = $userId->user_id;
+$userFirst = $userId->first_name;
+$userName = $userId->first_name . ' ' . $userId->last_name;
+$userEmail = $userId->email;
+
+$profile = new Profile();
+
+$results = $profile->getUserProfileIinfo($id);
+$age = $results->age;
+$weight = $results->weight;
+
+$img = $profile->getUserProfileImg($id)->profile_image;
